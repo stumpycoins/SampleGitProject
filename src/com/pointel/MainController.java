ff46d05f271e929cce0a518532d72d094240867e
Sivakumar M <sivakumar.mahadevan@gmail.com>
2022-04-28 14:44:14 +0530

    Version 1.2.0
    1.Multiplication feature added

--- src/com/pointel/MainController.java
+++ src/com/pointel/MainController.java
@@ -5,9 +5,13 @@ public class MainController {
 	private int addNumber(int a, int b) {
 		return a + b;
 	}
-	
+
 	private int subNumber(int a, int b) {
 		return a - b;
+	}
+
+	private int multipleNumber(int a, int b) {
+		return a * b;
 	}
 
 }
